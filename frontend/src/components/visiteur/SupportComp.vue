f55f489a8e00e40613fdc51f7ec08e3d7c40cdf9
Halazv2 <halaziani2003@gmail.com>
2022-04-27 09:27:17 +0000

    support page

---
+++ frontend/src/components/visiteur/SupportComp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <figure>
+      <img src="../../assets/images/support.png" alt="" />
+    </figure>
+    <div>
+      <div class="py-16 font-bold text-4xl text-center">
+        MAKE A DIFFERENCE <span class="text-primary-btn">DONATE</span>
+      </div>
+      <p class="w-2/3 text-left">
+        Help the animals by donating items from your home, you can donate
+        anything : food, pet clothes, money Please consider donating still
+        serviceable medical supplies to our Medical Donation Program
+      </p>
+      <h2 class="w-2/3 py-7 text-left text-xl font-bold">
+        Wish List Top Needs List
+      </h2>
+      <p class="w-2/3 text-left">
+        Canned dog and puppy food – any variety <br />
+        Canned cat and kitten food – any variety <br />
+        Kitten Milk Replacement <br />
+        Pet Ag Esbilac Powder (Puppy Milk Replacement) <br />
+        Snuggle Safe Heating Pads <br />
+        Universal Adjustable Dog Seatbelts <br />
+        Feliway Plugins and Refills<br />
+
+        Wobble Kongs <br />
+        Liter Boxes of all sizes <br />
+      </p>
+      <div class="py-16 flex justify-evenly items-center">
+        <p class="py-7 text-left text-xl font-bold">
+          You can Donate money from <br />
+          <a
+            href="https://www.patreon.com/posts/help-animals-by-65621966?utm_medium=clipboard_copy&utm_source=copy_to_clipboard&utm_campaign=postshare"
+            class="text-primary-btn"
+            >HERE (click me)</a
+          >
+        </p>
+        <img src="../../assets/images/screen.png" width="30%" alt="" />
+        <iframe
+          src="https://www.patreon.com/posts/help-animals-by-65621966?utm_medium=clipboard_copy&utm_source=copy_to_clipboard&utm_campaign=postshare"
+          allowfullscreen
+          width="400"
+          height="200"
+          title="sqd"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/views/SupportView.vue
+++ frontend/src/views/SupportView.vue
@@ -1,13 +1,14 @@
 <template>
   <center class="support">
-        <h1>This is a support page</h1>
-
+    <h1>This is a support page</h1>
+    <SupportComp />
   </center>
 </template>
 
 <script>
+import SupportComp from "../components/visiteur/SupportComp.vue";
 export default {
   name: "SupportView",
-  components: {},
+  components: { SupportComp },
 };
 </script>
