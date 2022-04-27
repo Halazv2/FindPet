51091bc9a88a680143c479e482f6aece31cca153
Halazv2 <halaziani2003@gmail.com>
2022-04-27 14:49:53 +0000

    AOS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,9 @@ import router from "./router";
 import store from "./store";
 import "./index.css";
 import 'flowbite';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 createApp(App).use(store).use(router).mount("#app");
