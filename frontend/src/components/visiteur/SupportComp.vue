1fd1f122f04db357db7d2b683f663a4ede3d644c
Halazv2 <halaziani2003@gmail.com>
2022-05-06 20:39:53 +0000

    support

--- frontend/src/components/visiteur/SupportComp.vue
+++ frontend/src/components/visiteur/SupportComp.vue
@@ -1,66 +1,69 @@
 <template>
   <div class="container">
     <figure>
-      <img src="../../assets/images/support.png" alt="" />
+      <img src="../../assets/images/support.png" class="px-11" alt="" />
     </figure>
     <div>
-      <div
-        class="
-          py-16
-          font-bold
-          text-4xl text-center
-          flex
-          items-center
-          justify-center
-          gap-6
-          flex-col
-          lg:flex-row
-        "
-      >
-        <h1>MAKE A DIFFERENCE <span class="text-primary-btn">DONATE</span></h1>
-      </div>
-      <div class="flex items-center  flex-col lg:flex-row justify-center">
-          <p class="w-[40%] text-left">
+      <div class="flex items-center flex-col lg:flex-row justify-center p-11">
+        <div class="lg:w-[45%] pb-7">
+          <div class="font-bold text-4xl flex flex-col lg:flex-row py-7">
+            <h1>
+              MAKE A DIFFERENCE <span class="text-primary-btn">DONATE</span>
+            </h1>
+          </div>
+          <p class="text-left">
             Help the animals by donating items from your home, you can donate
             anything : food, pet clothes, money Please consider donating still
             serviceable medical supplies to our Medical Donation Program
           </p>
-        <img src="../../assets/images/halfdog.png"  class="w-[50%] bg-primary-btn rounded-lg" alt="" />
-      </div>
-      <h2 class="w-2/3 py-7 text-left text-xl font-bold">
-        Wish List Top Needs List
-      </h2>
-      <p class="w-2/3 text-left">
-        Canned dog and puppy food – any variety <br />
-        Canned cat and kitten food – any variety <br />
-        Kitten Milk Replacement <br />
-        Pet Ag Esbilac Powder (Puppy Milk Replacement) <br />
-        Snuggle Safe Heating Pads <br />
-        Universal Adjustable Dog Seatbelts <br />
-        Feliway Plugins and Refills<br />
+        </div>
 
-        Wobble Kongs <br />
-        Liter Boxes of all sizes <br />
-      </p>
-      <div class="py-16 flex justify-evenly items-center">
-        <p class="py-7 text-left text-xl font-bold">
+        <img
+          src="../../assets/images/halfdog.png"
+          class="lg:w-[50%] bg-primary-btn rounded-lg"
+          alt=""
+        />
+      </div>
+      <div class="flex justify-center flex-col-reverse lg:flex-row gap-20 p-11">
+        <figure class=" bg-primary-btn rounded-lg">
+          <img src="../../assets/images/aa.png" class=" " alt="" />
+        </figure>
+        <div>
+          <h2 class=" py-7 text-left text-4xl font-bold">
+            <span class="text-primary-btn">Wish-List</span>
+             Top Needs List
+          </h2>
+          <p class=" text-left">
+            Canned dog and puppy food – any variety <br />
+            Canned cat and kitten food – any variety <br />
+            Kitten Milk Replacement <br />
+            Pet Ag Esbilac Powder (Puppy Milk Replacement) <br />
+            Snuggle Safe Heating Pads <br />
+            Universal Adjustable Dog Seatbelts <br />
+            Feliway Plugins and Refills<br />
+            Wobble Kongs <br />
+            Liter Boxes of all sizes <br />
+          </p>
+        </div>
+      </div>
+      <div class="py-16 flex flex-col p-11 justify-evenly items-center">
+        <h2 class="h2y-7 text-4xl font-bold text-center py-5">
           You can Donate money from <br />
           <a
             href="https://www.patreon.com/posts/help-animals-by-65621966?utm_medium=clipboard_copy&utm_source=copy_to_clipboard&utm_campaign=postshare"
             class="text-primary-btn"
             >HERE (click me)</a
           >
-        </p>
-        <!-- <img src="../../assets/images/screen.png" width="30%" alt="" /> -->
-        <iframe
+        </h2>
+        <img src="../../assets/images/screen.png" width="" alt="" />
+        <!-- <iframe
           src="https://www.patreon.com/FindPet/"
           allowfullscreen
           width="400"
           height="200"
           title="sqd"
           frameborder="0"
-        ></iframe>
-        <div></div>
+        ></iframe> -->
       </div>
     </div>
   </div>
@@ -78,4 +81,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h2 {
+  font-family: "Montserrat";
+  font-style: normal;
+}
+p {
+  font-family: "Montserrat";
+  font-style: normal;
+  font-size: 16px;
+  line-height: 24px;
+}
 </style>
