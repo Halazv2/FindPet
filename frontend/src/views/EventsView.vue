990c1b04809664e5a707da0e11ac26bccc0d8fb4
Halazv2 <halaziani2003@gmail.com>
2022-05-13 17:05:35 +0100

    start in frontend of event and feed view

---
+++ frontend/src/views/EventsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Feed page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: "FeedPage",
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/FeedView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      fqfsfsfs
+  </div>
+</template>
+
+<script>
+export default {
+name: "yry-trr"
+}
+</script>
+
+<style>
+
+</style>
